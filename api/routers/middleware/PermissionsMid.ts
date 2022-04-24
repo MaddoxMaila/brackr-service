be75918f0f3cb6806e6e735024a4279962f7bb1b
MaddoxMaila <maddoxmaila719@gmail.com>
2022-04-24 10:57:50 +0200

    done with the permissions middleware

--- api/routers/middleware/PermissionsMid.ts
+++ api/routers/middleware/PermissionsMid.ts
@@ -1,20 +1,60 @@
 import { NextFunction, Request, Response } from "express";
+import ErrorResponse from "../../libs/ErrorResponse";
 
 
+const UserTypes = {
+    user: 'user',
+    admin: 'admin',
+    superadmin: 'superadmin'
+}
 
-
-const PermissionsMid = (req: Request, res: Response, next: NextFunction) => {
+const AllAdminMid = (req: Request, res: Response, next: NextFunction) => {
 
     try{
 
-        
+        if(
+            (req.user?.type != UserTypes.admin) ||
+            (req.user?.type != UserTypes.superadmin)
+        ) throw new Error("Permission denied")
 
         next()
 
     }catch(e: any){
-
+        ErrorResponse(res, e)
     }
 
 }
 
-export default PermissionsMid
+const SuperAdminMid = (req: Request, res: Response, next: NextFunction) => {
+
+    try{
+
+        if(req.user?.type != UserTypes.superadmin) throw new Error("Permission denied")
+        
+        next()
+
+    }catch(e: any){
+        ErrorResponse(res, e)
+    }
+
+}
+
+const AdminMid = (req: Request, res: Response, next: NextFunction) => {
+
+    try{
+        
+        if(req.user?.type != UserTypes.admin) throw new Error("Permission denied")
+
+        next()
+
+    }catch(e: any){
+        ErrorResponse(res, e)
+    }
+
+}
+
+export {
+    SuperAdminMid,
+    AllAdminMid,
+    AdminMid
+}
