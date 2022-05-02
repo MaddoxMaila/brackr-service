18b7bcd035c51a8c33056f15b0dc28f58e2ecbe2
MaddoxMaila <maddoxmaila719@gmail.com>
2022-05-02 20:08:16 +0200

    added a settings screen

--- driver-client/src/paramLists/AppTabsParamList.ts
+++ driver-client/src/paramLists/AppTabsParamList.ts
@@ -1,4 +1,5 @@
 export type AppTabsParamList = {
     Home: undefined;
     Search: undefined;
+    Settings: undefined
   };
---
+++ driver-client/src/screens/settings.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+interface SettingsProps {}
+
+const Settings: React.FC<SettingsProps> = () => {
+    return (
+        <View>Settings</View>
+    )
+}
+
+export default Settings
