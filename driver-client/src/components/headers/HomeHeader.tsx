695f669188e1a862cd9349f15b42ea7e16d28f5a
MaddoxMaila <maddoxmaila719@gmail.com>
2022-05-15 15:23:31 +0200

    Now header reacts to what the uuser is doing... keeping track of user selections and showing them

--- driver-client/src/components/headers/HomeHeader.tsx
+++ driver-client/src/components/headers/HomeHeader.tsx
@@ -4,19 +4,37 @@ import Texter from "../base/Texter";
 import AppButton from "../base/Button";
 import {View, StyleSheet, Text, ScrollView} from 'react-native'
 import globalStyle from "../uiStyles/ui";
+import { useDispatch, useSelector } from "react-redux";
+import { JOURNEY_CONTEXT } from "../../constants";
 
 interface HomeHeaderProps {}
 
 const HomeHeader: React.FC<HomeHeaderProps> = () => {
+
+    const journey = useSelector((state: any) => state.journey)
+    const dispatch = useDispatch()
+
     return (
         <View style={[styles.wrapper, globalStyle.header]}>
             <Media
-                Body={ <Texter font={"text-max"} text={"Brackr"} /> } 
-                // Right={ <AppButton loading={false} block={false} title="Profile" /> }
+                Body={ 
+                <View>
+                    <Texter font={"text-max"} text={"Brackr"} />
+                    <View style={{paddingBottom: 8, paddingTop: 8}}>
+                        <Texter 
+                            font={"text-post"}
+                            text={
+                               journey.context == JOURNEY_CONTEXT.done ? "Create journey" : (journey.context == JOURNEY_CONTEXT.from ? "Choose starting location" : "Choose ending location")
+                            } />
+                    </View>
+                </View>
+             } 
+                Right={ 
+                <View style={{padding: 8}}>
+                    {/* <PopUp visible={false}/> */}
+                </View>
+            }
             />
-            <View style={{padding: 8}}>
-                <Texter font={"text-bold"} text={"Choose Starting Location"} />
-            </View>
         </View>
     )
 }
