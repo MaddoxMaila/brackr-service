63f53ddd11f5ddde2d0412d167be69559c0dd197
MaddoxMaila <maddoxmaila719@gmail.com>
2023-02-13 11:01:11 +0200

    added LOGGER for logging errors

--- brackr-api/libs/ApiResponse.ts
+++ brackr-api/libs/ApiResponse.ts
@@ -1,4 +1,11 @@
+import { LOGGER } from "./logger"
+
 export default function ApiResponse<T>(error: boolean, message: string, response: T) {
+    
+    if(error){
+        LOGGER("BRACKR-API", message)
+    }
+
     return {
         error, message, response
     }
