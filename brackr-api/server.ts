789236ac8d53f21e946f32a6f73dd49ead6b9c08
MaddoxMaila <maddoxmaila719@gmail.com>
2023-02-13 10:58:06 +0200

    refactored code, added connection checking to DB, RabbitMQ on app start

--- brackr-api/server.ts
+++ brackr-api/server.ts
@@ -3,15 +3,15 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieparser from 'cookie-parser'
 import helmet from 'helmet'
-import AuthMid from './routers/middleware/AuthMid'
-import ErrorMid from './routers/middleware/ErrorMid';
 
 //import routes
 import Routers from './routers/index';
-import ApiKeyMid from './routers/middleware/ApiKeyMid';
+import AuthMiddleware from './routers/middleware/AuthMiddleware'
+import ErrorMiddleware from './routers/middleware/ErrorMiddleware';
+import ApiKeyMiddleware from './routers/middleware/ApiKeyMiddleware';
 
 import socketIO from './socket.io'
-import celery from './tasks'
+import DatabaseSingleton from './prisma/DatabaseSingleton';
 
 
 //init
@@ -19,7 +19,7 @@ dotenv.config()
 
 const app = express()
 //middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieparser());
 app.use(helmet());
@@ -30,60 +30,34 @@ app.use(cors({ origin: true, credentials: true }))
 app.get('/', (req, res) => res.send('Running.. 🚀'))
 
 // Application middlewares
-const middleware = [
+const MIDDLEWARES = [
     // Makes sure requests that come have an access key to use our  services
-    AuthMid,
-    ApiKeyMid,
+    AuthMiddleware,
+    ApiKeyMiddleware,
 ]
 
-app.use(
-    '/auth',
-    ApiKeyMid,
-    Routers.authRouter
-)
-
-app.use(
-    '/admin',
-    middleware,
-    Routers.adminRouter
-)
-
-app.use(
-    '/journey',
-    middleware,
-    Routers.journeyRouter
-)
-
-app.use(
-    '/position',
-    middleware,
-    Routers.positionRouter,
-)
-
-app.use(
-    '/location',
-    middleware,
-    Routers.locationRouter
-)
+app.use('/auth', ApiKeyMiddleware, Routers.authRouter)
+app.use('/admin', MIDDLEWARES, Routers.adminRouter)
+app.use('/journey', MIDDLEWARES, Routers.journeyRouter)
+app.use('/position', MIDDLEWARES, Routers.positionRouter,)
+app.use('/location', MIDDLEWARES, Routers.locationRouter)
 
 // app.use('/auth', Routers.authRouter)
 // app.use('/private', AuthMid, require('./routers/authRouter'))
 
 
 // catch all error
-app.use(ErrorMid);
+app.use(ErrorMiddleware);
 
-
-(() => {
-
+const port = process.env.PORT || 2828;
+app.listen(port, async () => {
+    
+    console.log(`APPLICATION RUNNING ON PORT ${port}`)
+    
+    await DatabaseSingleton.connect()
+    
     // Initial socket IO connection
     socketIO.onConnection()
 
-    // Connect to rabbit/redis
-    celery.getCelery()
-
-})()
-
-const port = process.env.PORT || 2828;
-app.listen(port, () => console.log(`Server Running on PORT ${port}`))
+})
 
